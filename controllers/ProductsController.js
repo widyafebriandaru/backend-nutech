c8fadb711d6a2ca1ab4726071d9222112e5fbd08
widyafebriandaru <76604668+widyafebriandaru@users.noreply.github.com>
2023-08-18 00:22:34 +0700

    add alert

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -65,7 +65,7 @@ const updateProduct = async (req, res) => {
       harga_jual,
       stok,
     } = req.body;
-    if (req.accountType === "Admin") {
+    if (req.accountType !== "") {
       await db.product.update(
         {
           nama_barang: nama_barang,
--- router/productsRoute.js
+++ router/productsRoute.js
@@ -6,6 +6,6 @@ const { verifyUser, adminOnly } = require("../middleware/AuthUser");
 router.post("/products", createProduct);
 router.get("/products", getAllProducts);
 router.delete("/products/:id",verifyUser,adminOnly, deleteProduct );
-router.patch("/products/:id",verifyUser,updateProduct);
+router.patch("/products/:id", verifyUser, updateProduct);
 
 module.exports = router;
